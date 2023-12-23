ae881e4f60269edd7f3af260301c9ff9a0c7594f
suunzuu <sandle4535@gmail.com>
2023-12-23 17:37:49 +0900

    URL경로수정 및 AOS CSS추가

---
+++ hollys/js/common.js
@@ -0,0 +1,15 @@
+//**********
+//AOS
+//**********
+AOS.init();
+
+//**********
+//HEADER
+//**********
+let tnb = document.querySelector(".tnb");
+let allMenuBtn = document.querySelector(".all-menu-btn > span");
+
+allMenuBtn.addEventListener("click", function () {
+  this.textContent = this.textContent === "menu" ? "close" : "menu";
+  tnb.classList.toggle("active");
+});
--- hollys/js/header.js
+++
@@ -1,7 +0,0 @@
-let tnb = document.querySelector(".tnb");
-let allMenuBtn = document.querySelector(".all-menu-btn > span");
-
-allMenuBtn.addEventListener("click", function () {
-  this.textContent = this.textContent === "menu" ? "close" : "menu";
-  tnb.classList.toggle("active");
-});
