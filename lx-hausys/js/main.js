bb3c6085b31bd43a9fe499d7e04353bb417a2576
suunzuu <154397953+suunzuu@users.noreply.github.com>
2023-12-30 07:11:11 +0900

    [1차]LX하우시스 프로젝트 업로드

---
+++ lx-hausys/js/main.js
@@ -0,0 +1,10 @@
+// Initialize Swiper
+var swiper = new Swiper(".visualswiper", {
+  spaceBetween: 0,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
